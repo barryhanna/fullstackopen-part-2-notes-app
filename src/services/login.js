09ba6ba54c57e8ac13990a9860fcb2319cd54ae0
Barry Hanna <barryahanna@gmail.com>
2023-06-30 21:17:50 +0100

    Add login

---
+++ src/services/login.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const baseUrl = '/api/login'
+
+let token = null
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
+
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then((response) => response.data)
+}
+
+const login = async (credentials) => {
+  const response = await axios.post(baseUrl, credentials)
+  return response.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { login, setToken, create, update }
