ee1315bc4ccc4b584daf00d49860b013ea401be4
Barry Hanna <barryahanna@gmail.com>
2023-07-05 18:31:45 +0100

    More e2e tests

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,13 +1,13 @@
 describe('Note ', function () {
   beforeEach(function () {
-    cy.visit('http://localhost:3000')
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Matti Luukkainen',
       username: 'mluukkai',
       password: 'salainen',
     }
-    cy.request('POST', 'http://localhost:3001/api/users', user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
   })
 
   it('front page can be opened', function () {
@@ -26,21 +26,23 @@ describe('Note ', function () {
     cy.contains('Matti Luukkainen logged in')
   })
 
-  it.only('login fails with wrong password', function () {
+  it('login fails with wrong password', function () {
     cy.contains('Login').click()
     cy.get('#username').type('mluukkai')
     cy.get('#password').type('wrong')
     cy.get('#login-button').click()
 
     cy.get('.error').should('contain', 'Wrong credentials')
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
   })
 
   describe('when logged in', function () {
     beforeEach(function () {
-      cy.contains('Login').click()
-      cy.get('#username').type('mluukkai')
-      cy.get('#password').type('salainen')
-      cy.get('#login-button').click()
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+      // cy.contains('Login').click()
+      // cy.get('#username').type('mluukkai')
+      // cy.get('#password').type('salainen')
+      // cy.get('#login-button').click()
     })
 
     it('a new note can be created', function () {
@@ -52,17 +54,21 @@ describe('Note ', function () {
 
     describe('and a note exists', function () {
       beforeEach(function () {
-        cy.contains('New Note').click()
-        cy.get('input').type('another note cypress')
-        cy.contains('save').click()
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
       it('it can be made important', function () {
-        cy.contains('another note cypress')
-          .contains('make not important')
-          .click()
+        cy.contains('second note').contains('make important').click()
 
-        cy.contains('another note cypress').contains('make important')
+        cy.contains('second note').contains('make not important')
+      })
+
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
       })
     })
   })
